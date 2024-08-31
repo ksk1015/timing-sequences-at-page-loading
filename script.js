ab1e74f8d6ca5a0b74de2bd1f3c641a77f3dc841
ksk1015 <ksk.aizawa@gmail.com>
2024-08-31 23:43:31 +0900

    tweak style and text

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setLog(
   `${
     document.currentScript
-      ? document.currentScript.src.split('?name=')[1]
-      : 'head-module'
+      ? document.currentScript.src.split('?name=')[1].replace('-', ' ')
+      : 'head module'
   } script`
 )
